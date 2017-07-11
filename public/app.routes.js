4c4fbb1d5b24412a0141c9c1a23e665913854c57
Sean McDermott <mcderm.sean@gmail.com>
2017-07-11 10:50:28 -0700

    added UI router and dyanmic route to a room

---
+++ public/app.routes.js
@@ -0,0 +1,26 @@
+(function() {
+  'use strict';
+
+  angular.module('app').config(config)
+
+  config.$inject = ['$stateProvider', '$urlRouterProvider', '$locationProvider']
+
+  function config($stateProvider, $urlRouterProvider, $locationProvider){
+
+    // enables cleaner URLs
+    $locationProvider.html5Mode(true)
+
+    $stateProvider
+      .state({
+        name: 'home',
+        url: '/',
+        component: 'cloudsound',
+      })
+      .state({
+        name: 'studio',
+        url: '/:room',
+        component: 'studio',
+      })
+  }
+
+}());
