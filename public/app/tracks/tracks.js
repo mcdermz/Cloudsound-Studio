606062b251db8856ee50443f7b8e51619899258a
Sean McDermott <mcderm.sean@gmail.com>
2017-07-12 15:42:04 -0700

    user can play a sound!

--- public/app/tracks/tracks.js
+++ public/app/tracks/tracks.js
@@ -5,9 +5,48 @@
       templateUrl: './app/tracks/tracks.html',
     })
 
-  controller.$inject = ['socketService', '$state']
-  function controller(socketService, $state) {
+  controller.$inject = ['socketService', '$state', '$http']
+  function controller(socketService, $state, $http) {
     const vm = this
     const socket = socketService.socket
+
+    window.AudioContext = window.AudioContext||window.webkitAudioContext
+
+    const ctx = new AudioContext()
+    const masterGain = ctx.createGain()
+
+    masterGain.gain.value = 0
+    masterGain.connect(ctx.destination)
+
+    let source
+
+    vm.getData = function() {
+      source = ctx.createBufferSource()
+      $http({
+          method: 'GET',
+          url: '/audio/tone-samples.mp3',
+          responseType: 'arraybuffer'
+      }).then(response => {
+          ctx.decodeAudioData(response.data)
+          .then(buffer => {
+            source.buffer = buffer
+            source.connect(ctx.destination);
+            source.loop = true;
+          })
+      }).catch(err => {
+        console.error(err);
+      })
+    }
+
+
+    vm.play = function (){
+      vm.getData();
+      source.start(0);
+    }
+
+    vm.stop = function (){
+      source.stop()
+    }
+
   }
 })()
