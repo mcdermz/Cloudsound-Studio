460e5e0a330dc86f937be998a77add59b1f656b2
Sean McDermott <mcderm.sean@gmail.com>
2017-07-13 11:31:58 -0700

    play/stop actions enabled through sockets

--- public/app/tracks/tracks.js
+++ public/app/tracks/tracks.js
@@ -3,12 +3,16 @@
     .component('tracks', {
       controller: controller,
       templateUrl: './app/tracks/tracks.html',
+      bindings: {
+        trackName: '@'
+      },
     })
 
-  controller.$inject = ['socketService', 'audioService', '$state']
+  controller.$inject = ['socketService', 'audioService', '$state', '$scope']
 
-  function controller(socketService, audioService, $state) {
+  function controller(socketService, audioService, $state, $scope){
     const vm = this
+    const socket = socketService.socket
     const ctx = audioService.ctx
     const gainNode = ctx.createGain()
     gainNode.gain.value = 0
@@ -16,6 +20,29 @@
     const url = '/audio/tone-samples.mp3'
 
     const track = { gainNode, url }
+
+    vm.$onInit = function() {
+      const socketPlayEvent = 'play ' + vm.trackName
+      vm.sendPlayMessage = function (){
+        socket.emit('play track', socketPlayEvent)
+      }
+      socket.on('play track', function(msg){
+        $scope.$apply(function() {
+          vm.play()
+        })
+      })
+
+      const socketStopEvent = 'stop ' + vm.trackName
+      vm.sendStopMessage = function (){
+        socket.emit('stop track', socketStopEvent)
+      }
+
+      socket.on('stop track', function(msg){
+        $scope.$apply(function() {
+          vm.stop()
+        })
+      })
+    }
 
     vm.play = function (){
       vm.playing = true
