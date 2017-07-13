f32ddbf70abc675d24c036e0581f0082e5bc4340
Sean McDermott <mcderm.sean@gmail.com>
2017-07-13 09:56:46 -0700

    abstracted getData() to audio-service.js

--- public/app/audio-service.js
+++ public/app/audio-service.js
@@ -5,9 +5,35 @@
 
     .service('audioService', service)
 
-    service.$inject = []
+    service.$inject = ['$http']
 
-    function service(){
-      
+    function service($http){
+      window.AudioContext = window.AudioContext||window.webkitAudioContext
+
+      this.ctx = new AudioContext()
+      this.masterGain = this.ctx.createGain()
+
+      this.masterGain.gain.value = 0
+      this.masterGain.connect(this.ctx.destination)
+
+      this.getData = async function(track) {
+        const urlPkg = {
+          method: 'GET',
+          url: track.url,
+          responseType: 'arraybuffer'
+        }
+
+        track.source = this.ctx.createBufferSource()
+        try {
+          let audioData = await $http(urlPkg)
+          let buffer = await this.ctx.decodeAudioData(audioData.data)
+          track.source.buffer = buffer
+          track.source.connect(this.masterGain);
+          track.source.loop = true;
+        }
+        catch (error) {
+          console.error(error);
+        }
+      }
     }
 })()
--- public/app/tracks/tracks.js
+++ public/app/tracks/tracks.js
@@ -5,54 +5,24 @@
       templateUrl: './app/tracks/tracks.html',
     })
 
-  controller.$inject = ['socketService', '$state', '$http']
-  function controller(socketService, $state, $http) {
+  controller.$inject = ['socketService', 'audioService', '$state']
+  function controller(socketService, audioService, $state) {
     const vm = this
-
-    window.AudioContext = window.AudioContext||window.webkitAudioContext
-
-    const ctx = new AudioContext()
-    const masterGain = ctx.createGain()
-
-    masterGain.gain.value = 0
-    masterGain.connect(ctx.destination)
-
-
     const track = {
       url: '/audio/tone-samples.mp3',
     }
 
-    vm.getData = async function(track) {
-      const urlPkg = {
-        method: 'GET',
-        url: track.url,
-        responseType: 'arraybuffer'
-      }
-
-      track.source = ctx.createBufferSource()
-      try {
-        let audioData = await $http(urlPkg)
-        let buffer = await ctx.decodeAudioData(audioData.data)
-        track.source.buffer = buffer
-        track.source.connect(masterGain);
-        track.source.loop = true;
-      }
-      catch (error) {
-        console.error(error);
-      }
-    }
-
     vm.play = function (){
       vm.playing = true
-      vm.getData(track);
+      audioService.getData(track);
       track.source.start(0);
-      masterGain.gain.value = 1
+      audioService.masterGain.gain.value = 1
     }
 
     vm.stop = function (){
       vm.playing = false
-      masterGain.gain.value = 0
-      track.source.stop(ctx.currentTime + 0.1)
+      audioService.masterGain.gain.value = 0
+      track.source.stop(audioService.ctx.currentTime + 0.1)
     }
 
   }
