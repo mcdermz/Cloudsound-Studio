057196eaeb0ef4fa1ff0186b175bed424e8ea609
Sean McDermott <mcderm.sean@gmail.com>
2017-07-16 20:22:56 -0700

    added studio-service.js file

---
+++ public/app/studio/studio-service.js
@@ -0,0 +1,12 @@
+(function (){
+  'use strict'
+
+  angular.module('app')
+  .service('studioService', service)
+
+  service.$inject = []
+
+  function service(){
+    this.soloedTracks = 0
+  }
+})()
