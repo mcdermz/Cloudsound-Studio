9269ce2e8446031e52ac2c69d9e7944d2b17afe8
Sean McDermott <mcderm.sean@gmail.com>
2017-07-19 11:41:49 -0700

    added trackNames array to studioService

--- public/app/studio/studio-service.js
+++ public/app/studio/studio-service.js
@@ -7,6 +7,23 @@
   service.$inject = []
 
   function service(){
+    this.trackNames = [{
+        name: 'one',
+        srcAudioUrl: 'https://s3-us-west-2.amazonaws.com/cloudsoundstudio/drums/80s-back-beat-01.mp3'
+      },
+      {
+        name: 'two',
+        srcAudioUrl: 'https://s3-us-west-2.amazonaws.com/cloudsoundstudio/bass/british-blues-bass.mp3'
+      },
+      {
+        name: 'three',
+        srcAudioUrl: 'https://s3-us-west-2.amazonaws.com/cloudsoundstudio/rhythm/disco-delight-clav.mp3'
+      },
+      {
+        name: 'four',
+        srcAudioUrl: 'https://s3-us-west-2.amazonaws.com/cloudsoundstudio/rhythm/drop-the-funk-lead-guitar.mp3'
+      }
+    ]
     this.soloedTracks = 0
     this.onOccupy = function(controller, parameter, bool) {
       return function(msg) {
