61260b2db1e270f0916e77a68e48cf026be88f1f
Sean McDermott <mcderm.sean@gmail.com>
2017-07-20 11:17:19 -0700

    able to get track list from s3 converted to an array of url endpoints

--- public/app/studio/studio.js
+++ public/app/studio/studio.js
@@ -5,15 +5,18 @@
       templateUrl: './app/studio/studio.html',
     })
 
-  controller.$inject = ['socket', 'visualizerService', 'studioService', '$state']
-  function controller(socket, visualizerService, studioService, $state) {
+  controller.$inject = ['socket', 'visualizerService', 'studioService', '$state', '$http']
+  function controller(socket, visualizerService, studioService, $state, $http) {
     const vm = this
     const room = $state.params.room
 
-    vm.$onInit = function(){
+    vm.$onInit = async function(){
       vm.createRoom(room)
       vm.trackNames4 = studioService.trackNames.slice(0, 4);
       vm.trackNames8 = studioService.trackNames.slice(4)
+
+      vm.getTracks = await getAvailableTracks()
+      console.log(vm.getTracks);
     }
 
     vm.createRoom = function(name){
@@ -27,15 +30,25 @@
 
     vm.eightTrackToggle = function() {
       const is8Track = vm.is8Track || false
-      console.log(vm.is8Track);
       const data = { room, is8Track }
       socket.emit('8 track toggle', data)
     }
 
+    const getAvailableTracks = async function() {
+      const x2js = new X2JS();
+      const getTracksPkg = {
+        method: 'GET',
+        url: 'https://s3-us-west-2.amazonaws.com/cloudsoundstudio',
+      }
+      const availableTracksXml = await $http(getTracksPkg)
+      const availableTracksJSON = x2js.xml_str2json(availableTracksXml.data)
+      const bucketContents = availableTracksJSON.ListBucketResult.Contents
+      const availableTracks = bucketContents.filter(obj => obj.Size !== '0').map(obj => obj.Key);
+      return availableTracks
+    }
+
     socket.on('8 track toggle', function(msg) {
-      console.log(msg);
       vm.is8Track = msg.is8Track
-      console.log(vm.is8Track);
     })
   }
 })()
