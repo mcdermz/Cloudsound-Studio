17f9381cbe7f9bde1e54c37fd591238a1cf00824
Sean McDermott <mcderm.sean@gmail.com>
2017-07-20 09:55:43 -0700

    added ng-repeat to both 4 and 8 track settings

--- public/app/studio/studio.js
+++ public/app/studio/studio.js
@@ -5,13 +5,18 @@
       templateUrl: './app/studio/studio.html',
     })
 
-  controller.$inject = ['socket', 'visualizerService', '$state']
-  function controller(socket, visualizerService, $state) {
+  controller.$inject = ['socket', 'visualizerService', 'studioService', '$state']
+  function controller(socket, visualizerService, studioService, $state) {
     const vm = this
 
     vm.$onInit = function(){
       const roomName = $state.params.room
       vm.createRoom(roomName)
+      vm.trackNames4 = studioService.trackNames.slice(0, 4);
+      vm.trackNames8 = studioService.trackNames.slice(4)
+
+      console.log('First four: ', vm.trackNames4);
+      console.log('Last four: ', vm.trackNames8);
     }
 
     vm.createRoom = function(name){
